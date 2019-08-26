e068f38d657f637536bd80964f214b908a6ad348
Gustavo Santos <gustavo.santos@ccc.ufcg.edu.br>
2019-08-25 22:24:38 -0300

    Initial commit

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '357ada4f1258e8777d2dfec6dacb32d1',
+  expiresIn: '7d',
+};
---
+++ src/routes.js
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+const upload = multer(multerConfig);
+
+// Routes without Middleware
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+// GET
+routes.get('/providers', ProviderController.index);
+routes.get('/appointments', AppointmentController.index);
+routes.get('/schedule', ScheduleController.index);
+
+// POST
+routes.post('/files', upload.single('file'), FileController.store);
+routes.post('/appointments', AppointmentController.store);
+
+// PUT
+routes.put('/users', UserController.update);
+
+// DELETE
+
+export default routes;
