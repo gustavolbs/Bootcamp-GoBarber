f63e312cefbe9ea79322ddf89b5b6b9902adc5b0
Gustavo Santos <gustavo.santos@ccc.ufcg.edu.br>
2019-08-25 23:45:42 -0300

    feature: Added notifications and mail on delete

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'ffaf2d2c5efa70',
+    pass: '6f8d9fec1f19bc',
+  },
+  default: {
+    from: 'Equipe GoBarber <no-reply@gobarber.com>',
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -24,6 +25,7 @@ routes.use(authMiddleware);
 routes.get('/providers', ProviderController.index);
 routes.get('/appointments', AppointmentController.index);
 routes.get('/schedule', ScheduleController.index);
+routes.get('/notifications', NotificationController.index);
 
 // POST
 routes.post('/files', upload.single('file'), FileController.store);
@@ -31,7 +33,9 @@ routes.post('/appointments', AppointmentController.store);
 
 // PUT
 routes.put('/users', UserController.update);
+routes.put('/notifications/:id', NotificationController.update);
 
 // DELETE
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 export default routes;
