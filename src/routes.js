d6d1e4c2f429e186a101c6d1a6a916db708e8f42
Gustavo Santos <gustavo.santos@ccc.ufcg.edu.br>
2019-08-26 23:30:12 -0300

    feature: Added Availability, sentry, environment variables

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,13 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
+import FileController from './app/controllers/FileController';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
-import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
+import AvailableController from './app/controllers/AvailableController';
+import AppointmentController from './app/controllers/AppointmentController';
 import NotificationController from './app/controllers/NotificationController';
 
 import authMiddleware from './app/middlewares/auth';
@@ -23,6 +24,7 @@ routes.use(authMiddleware);
 
 // GET
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 routes.get('/appointments', AppointmentController.index);
 routes.get('/schedule', ScheduleController.index);
 routes.get('/notifications', NotificationController.index);
